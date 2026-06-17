4cb7ee5f777b42381900aebe0baa41685c90632c
gourav <gouravsharma20a@gmail.com>
2026-06-17 15:26:15 +0530

    feature:get all movies api made

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(express.json())
 
 const fs = require("fs")
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
 
 const Movie = require("./model/MovieModel.js")
 const { error } = require("console")
@@ -17,30 +27,41 @@ const { error } = require("console")
 const jsonData = fs.readFileSync("./data/hotelData.json","utf-8")
 
 app.get("/",(req,res)=>{
-    res.json("Welcoem to my express app")
+    res.json("Welcoem to my Movie express app")
 })
 
-// async function createMovie(newMovie) {
-//     await initializeDatabase()
-//   try {
-//     const movie = new Movie(newMovie)
-//     const saveMovie = await movie.save()
-//     return saveMovie
-//   } catch (error) {
-//     throw error
-//   }
-// }
+async function createMovie(newMovie) {
+    await initializeDatabase()
+  try {
+    const movie = new Movie(newMovie)
+    const saveMovie = await movie.save()
+    return saveMovie
+  } catch (error) {
+    throw error
+  }
+}
 
 async function getAllMovie() {
     try {
         const movies = await Movie.find()
-        console.log(movies)
+        return movies
     } catch(err){
         console.log("an error occured while fetching movies",err)
     }
 }
 
-getAllMovie()
+app.get("/allMovies",async(req,res)=>{
+    try {
+        const allMovies = await getAllMovie()
+        if (!allMovies && allMovies.length === 0) {
+            return res.status(404).json({error:"all movies not founds"})
+        } else {
+            return res.status(200).json({message:"all movies found successfully",AllMovies:allMovies})
+        }
+    } catch (err) {
+        return res.status(500).json({error:"an error occured while getting movies",errorDetails:err.message})
+    }
+})
 
 
 async function updateMovie(movieId,dataToUpdate) {
@@ -70,24 +91,24 @@ app.post("/movies/:movieId",async(req,res)=>{
 
 
 
-// app.post("/movies",async(req,res)=>{
-//     console.log("POST HITTED")
-//     try{
-//         const savedMovie = await createMovie(req.body)
-//         console.log(savedMovie)
-//         return res.status(201).json({message:"Movie added successfully",movie:savedMovie})
+app.post("/movies",async(req,res)=>{
+    console.log("POST HITTED")
+    try{
+        const savedMovie = await createMovie(req.body)
+        console.log(savedMovie)
+        return res.status(201).json({message:"Movie added successfully",movies:savedMovie})
 
-//     } catch(err){
-//         res.status(500).json({error:"failed to add movie",errDetails:err.message})
-//         console.log(err.message)
-//     }
-// })
+    } catch(err){
+        res.status(500).json({error:"failed to add movie",errDetails:err.message})
+        console.log(err.message)
+    }
+})
 
 
 const hotelData = JSON.parse(jsonData)
 
 
-const PORT = 2222
+const PORT = 2225
 
 app.listen(PORT,()=>{
     console.log(`Server is running on Port ${PORT}`)
